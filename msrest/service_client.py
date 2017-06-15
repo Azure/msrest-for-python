fe8a2e93bb5fb78927ec164cbdd8be158f3b9b4e
Laurent Mazuel <lmazuel@microsoft.com>
2017-06-15 16:39:55 -0700

    Return the result of the cb

--- msrest/service_client.py
+++ msrest/service_client.py
@@ -134,7 +134,7 @@ class ServiceClient(object):
         def make_user_hook_cb(user_hook, session):
             def user_hook_cb(r, *args, **kwargs):
                 kwargs.setdefault("msrest", {})['session'] = session
-                user_hook(r, *args, **kwargs)
+                return user_hook(r, *args, **kwargs)
             return user_hook_cb
 
         for user_hook in self.config.hooks:
