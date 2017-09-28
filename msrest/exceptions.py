2ef87eae15401307be38f435334af7be790bd71d
Laurent Mazuel <laurent.mazuel@gmail.com>
2017-09-28 12:20:37 -0700

    Fix #46 - best except

--- msrest/exceptions.py
+++ msrest/exceptions.py
@@ -136,7 +136,13 @@ class HttpOperationError(ClientException):
                 self.error = deserialize(resp_type, response)
                 if self.error is None:
                     self.error = deserialize.dependencies[resp_type]()
-                self.message = self.error.message
+                # ARM uses OData v4
+                # http://docs.oasis-open.org/odata/odata-json-format/v4.0/os/odata-json-format-v4.0-os.html#_Toc372793091
+                # Code and Message are REQUIRED
+                self.message = "({}) {}".format(
+                    self.error.error.code,
+                    self.error.error.message
+                )
         except (DeserializationError, AttributeError, KeyError):
             pass
 
