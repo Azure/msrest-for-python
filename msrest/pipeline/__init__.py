71c749fbdfc2c04ec35778bb82664753b24a211a
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-07-06 17:11:07 -0700

    Add absolute_import for Py2.7

--- msrest/pipeline/__init__.py
+++ msrest/pipeline/__init__.py
@@ -23,6 +23,7 @@
 # IN THE SOFTWARE.
 #
 # --------------------------------------------------------------------------
+from __future__ import absolute_import  # we have a "requests" module that conflicts with "requests" on Py2.7
 import abc
 import json
 import logging
