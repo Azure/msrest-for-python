d494cff21317a9d9aa348629036fa809a0a74a51
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-08-31 13:32:32 -0700

    msrest 0.5.5

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='msrest',
-    version='0.5.4',
+    version='0.5.5',
     author='Microsoft Corporation',
     packages=find_packages(exclude=["tests", "tests.*"]),
     url=("https://github.com/Azure/msrest-for-python"),
