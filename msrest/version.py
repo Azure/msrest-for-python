c25b23b871301dd94d5bf16b9cfba98460f94eb2
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-09-26 09:01:00 -0700

    0.6.0rc1

--- msrest/version.py
+++ msrest/version.py
@@ -25,4 +25,4 @@
 # --------------------------------------------------------------------------
 
 #: version of this package. Use msrest.__version__ instead
-msrest_version = "0.5.5"
+msrest_version = "0.6.0rc1"
